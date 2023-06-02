642beb2d0a373f84e230deb14f73286fb312e5e7
SanjayPrakash1371 <118902436+SanjayPrakash1371@users.noreply.github.com>
2023-06-02 19:28:08 +0530

    Create Qs.py
    QuickSort

---
+++ Qs.py
@@ -0,0 +1,42 @@
+def quickSort(low,high,nums):
+    if(low<high):
+        pI=partition(low,high,nums)
+        quickSort(low,pI-1,nums)
+        quickSort(pI+1,high,nums)
+
+
+
+def partition(low,high,nums):
+
+    pivot=nums[low]
+
+    i=low;
+    j=high;
+
+    while i < j:
+        while i<=high and nums[i]<=pivot:
+            i+=1
+        while j>=low and nums[j]>pivot:
+            j-=1
+        if i<j:
+            temp=nums[i]
+            nums[i]=nums[j]
+            nums[j]=temp
+
+
+    temp=nums[low]
+    nums[low]=nums[j]
+    nums[j]=temp
+
+    print(nums,j)
+
+    return j
+
+
+
+
+lst=[5,4,3,2,1]
+
+quickSort(0,len(lst)-1,lst)
+
+print(lst)
